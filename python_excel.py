37e4d838b922334713b410bd9cd0a4cf4f48584a
wys <wys920811@163.com>
2017-12-27 02:04:52 +0800

    support new format

--- python_excel.py
+++ python_excel.py
@@ -5,6 +5,8 @@ import xlutils
 import os
 import re
 import time
+from decimal import Decimal 
+from decimal import getcontext
 
 #定义两个全局list，all_info存储每个excel内容，bank_info存储表格内银行名，以便后面根据银行名来确认写excel时输出位置
 all_infos = []
@@ -15,8 +17,9 @@ def read_excel_pufa(file_name):
 	wb = xlrd.open_workbook(file_name)
 	sh = wb.sheet_by_index(0)
 	count = sh.cell(0,1).value #账号
-	count_name = sh.cell(1,1).value #账户名
-	l_pufa = ['浦发银行' + file_name.replace('.xls',''),count] #银行名和账号放进一个临时list，后面把所有信息一起作为一个list，append进all_info，后面取值方便
+	count_name = sh.cell(1,1).value #账户名 
+	bank_type = file_name.replace('.xls','')[2:]
+	l_pufa = [bank_type,count] #银行名和账号放进一个临时list，后面把所有信息一起作为一个list，append进all_info，后面取值方便
 	for i in range(sh.nrows):
 		if i >= 4:
 			if sh.row_values(i)[0] != '':			
@@ -52,7 +55,8 @@ def read_excel_jianhang(file_name):
 	bank_name = sh.cell(3,1).value
 	count = sh.cell(4,1).value
 	count_name = sh.cell(5,1).value
-	l_jianhang = ['建设银行' + file_name.replace('.xls',''),count]
+	bank_type = file_name.replace('.xls','')[2:]
+	l_jianhang = [bank_type,count]
 	# global bank_info
 	# bank_info.append({'bank_name':'建行','count':count})
 	for i in range(sh.nrows):
@@ -85,7 +89,8 @@ def read_excel_zhaohang(file_name):
 	bank_name = '招商银行'
 	count = ''
 	count_name = ''
-	l_zhaohang = ['招商银行' + file_name.replace('.xls',''),count]
+	bank_type = file_name.replace('.xlsx','')[2:]
+	l_zhaohang = [bank_type,count]
 	# global bank_info
 	# bank_info.append({'bank_name':'建行','count':count})
 	for i in range(sh.nrows):
@@ -116,7 +121,8 @@ def read_excel_zhongxin(file_name):
 	sh = wb.sheet_by_index(0)
 	count = sh.cell(1,3).value #账号
 	count_name = sh.cell(1,1).value #账户名
-	l_zhongxin = ['中信银行' + file_name.replace('.xls',''),count] #银行名和账号放进一个临时list，后面把所有信息一起作为一个list，append进all_info，后面取值方便
+	bank_type = file_name.replace('.xls','')[2:]
+	l_zhongxin = [bank_type,count] #银行名和账号放进一个临时list，后面把所有信息一起作为一个list，append进all_info，后面取值方便
 	for i in range(sh.nrows):
 		if i >= 4:
 			if sh.row_values(i)[0] != '':			
@@ -155,6 +161,12 @@ def write_excel(all_infos):
 	
 
 	style_title = xlwt.XFStyle() # 标题加粗居中宋体
+	borders= xlwt.Borders()
+	borders.left = 1
+	borders.right = 1
+	borders.top = 1
+	borders.bottom = 1
+	style_title.borders = borders
 	alignment = xlwt.Alignment()
 	alignment.horz = xlwt.Alignment.HORZ_CENTER
 	alignment.vert = xlwt.Alignment.VERT_CENTER
@@ -196,42 +208,72 @@ def write_excel(all_infos):
 	sheet.write_merge(3, 4, 0, 0, '月',style_title)
 	sheet.write_merge(3, 4, 1, 1, '日',style_title)
 	sheet.write_merge(3, 4, 2, 2, '编号',style_title)
-	sheet.write_merge(1, 3, 3, 3, '摘要',style_title)
-	sheet.write_merge(1, 3, 4, 4, '银行性质（公私）',style_title)
-	sheet.write_merge(1, 3, 5, 5, '银行名称',style_title)
-	sheet.write_merge(1, 3, 6, 6, '性质（收付）',style_title)
-	sheet.write_merge(1, 3, 7, 7, '部门',style_title)
-	sheet.write_merge(1, 3, 8, 10, '合计',style_title)
-	sheet.write(4, 8, '收',style_title)
-	sheet.write(4, 9, '付',style_title)
-	sheet.write(4, 10, '结存',style_title)
-	sheet.write_merge(1, 1, 14, 13+3*len(all_infos), '银行存款',style_title)#
+	sheet.write_merge(1, 4, 3, 3, '摘要',style_title)
+	sheet.write_merge(1, 4, 4, 4, '银行名称',style_title)
+	sheet.write_merge(1, 4, 5, 5, '类型',style_title)
+	sheet.write_merge(1, 3, 6, 8, '合计',style_title)
+	sheet.write_merge(1, 3, 9, 11, '库存现金',style_title)
+
+	sheet.write(4, 6, '收',style_title)
+	sheet.write(4, 7, '付',style_title)
+	sheet.write(4, 8, '结存',style_title)
+	sheet.write(4, 9, '收',style_title)
+	sheet.write(4, 10, '付',style_title)
+	sheet.write(4, 11, '结存',style_title)
+	sheet.write_merge(1, 1, 12, 11+3*len(all_infos), '银行存款',style_title)#
 	info_count = 0 #初始化一个变量来保存已经写入的行数，因为不同银行所在列不同
-	
+	all_money_ins = []
+	all_money_outs = []
+	all_money_nows = []
 	for i in range(len(all_infos)):
-		sheet.write_merge(2, 2, 14+3*i, 16+3*i, all_infos[i][0][6:],style_title)
-		sheet.write_merge(3, 3, 14+3*i, 16+3*i, all_infos[i][1],style_title)
-		sheet.write(4, 14+3*i, '收',style_title)
-		sheet.write(4, 15+3*i, '付',style_title)
-		sheet.write(4, 16+3*i, '结存',style_title)
+		sheet.write_merge(2, 2, 12+3*i, 14+3*i, all_infos[i][0][0:4],style_title)
+		sheet.write_merge(3, 3, 12+3*i, 14+3*i, all_infos[i][1],style_title)
+		sheet.write(4, 12+3*i, '收',style_title)
+		sheet.write(4, 13+3*i, '付',style_title)
+		sheet.write(4, 14+3*i, '结存',style_title)
+		# all_money_ins = all_money_ins + all_infos[i][-1]['money_in'] if all_infos[i][-1]['money_in'] != '' else 0
+		# all_money_outs = all_money_ins + all_infos[i][-1]['money_out'] if all_infos[i][-1]['money_in'] != '' else 0
+		# all_money_nows = all_money_ins + all_infos[i][-1]['money_now'] if all_infos[i][-1]['money_in'] != '' else 0
+
+		all_money_in = Decimal('0.00')
+		all_money_out = Decimal('0.00')
+
 		for j in range(len(all_infos[i]) - 2):
 			#下面需要通过正则匹配获取日期中正整数，如20170102，取出01时要输出1
 			sheet.write(5+j + info_count, 0, re.findall(r"[1-9]\d*",str(all_infos[i][j+2]['date'])[4:6]),style_num_align) 
 			sheet.write(5+j + info_count, 1, re.findall(r"[1-9]\d*",str(all_infos[i][j+2]['date'])[6:8]),style_num_align)
+			sheet.write(5+j + info_count, 2, j + info_count + 1,style_num_align)
 			sheet.write(5+j + info_count, 3, all_infos[i][j+2]['beizhu'],style_content)
-			sheet.write(5+j + info_count, 4, '公账',style_content)
-			sheet.write(5+j + info_count, 5, all_infos[i][0][0:4],style_content)
+			sheet.write(5+j + info_count, 4, all_infos[i][0][0:4],style_content)
+			sheet.write(5+j + info_count, 5, all_infos[i][0][4:],style_content)
 
 			#第一个参数，行，j为其中一个银行流水线数据中第j行，第一个银行数据输入完后，要把行数存到info_count，下一个银行要从下面继续输入，
 			#因为不同银行的同一类型数据要写在不同行不同列，第二个参数也要自动确认列数
-			sheet.write(5+j + info_count, 14+3*i, all_infos[i][j+2]['money_in'],style_num)
-			sheet.write(5+j + info_count, 15+3*i, all_infos[i][j+2]['money_out'],style_num)
-			sheet.write(5+j + info_count, 16+3*i, all_infos[i][j+2]['money_now'],style_num)
-			sheet.write(5+j + info_count, 8, all_infos[i][j+2]['money_in'],style_num)
-			sheet.write(5+j + info_count, 9, all_infos[i][j+2]['money_out'],style_num)
-			sheet.write(5+j + info_count, 10, all_infos[i][j+2]['money_now'],style_num)
+			sheet.write(5+j + info_count, 12+3*i, all_infos[i][j+2]['money_in'],style_num)
+			sheet.write(5+j + info_count, 13+3*i, all_infos[i][j+2]['money_out'],style_num)
+			sheet.write(5+j + info_count, 14+3*i, all_infos[i][j+2]['money_now'],style_num)
+			sheet.write(5+j + info_count, 6, all_infos[i][j+2]['money_in'],style_num)
+			sheet.write(5+j + info_count, 7, all_infos[i][j+2]['money_out'],style_num)
+			sheet.write(5+j + info_count, 8, all_infos[i][j+2]['money_now'],style_num)
+			all_money_in = all_money_in + Decimal(all_infos[i][j+2]['money_in'] if all_infos[i][j+2]['money_in'] !='' else 0)
+			all_money_out = all_money_out + Decimal(all_infos[i][j+2]['money_out'] if all_infos[i][j+2]['money_out'] !='' else 0)
+			all_money_now = Decimal(all_infos[i][j+2]['money_now'] if all_infos[i][j+2]['money_now'] !='' else 0)
+		all_money_ins.append({'all_money_in':all_money_in,'all_money_out':all_money_out,'all_money_now':all_money_now})		
 		info_count = info_count + len(all_infos[i]) - 2
 
+	money_ins = Decimal('0.00')
+	money_outs = Decimal('0.00')
+	money_nows = Decimal('0.00')
+	for k in range(len(all_money_ins)):
+		money_ins = money_ins + Decimal((all_money_ins[k]['all_money_in']))
+		money_outs = money_outs + Decimal((all_money_ins[k]['all_money_out']))
+		money_nows = money_nows + Decimal((all_money_ins[k]['all_money_now']))
+		sheet.write(5 + info_count, 12 + k * 3, float(all_money_ins[k]['all_money_in']),style_num)
+		sheet.write(5 + info_count, 13 + k * 3, float(all_money_ins[k]['all_money_out']),style_num)
+		sheet.write(5 + info_count, 14 + k * 3, float(all_money_ins[k]['all_money_now']),style_num)
+	sheet.write(5 + info_count, 3, '合计',style_title)
+	sheet.write(5 + info_count, 7, float(money_outs),style_num)
+	sheet.write(5 + info_count, 8, float(money_nows),style_num)
 	wbk.save('日记账' + time.strftime('%Y%m%d%H%M%S',time.localtime()) + '.xls') #循环输入后保存，此时的文件名对应的文件可以存在，会被覆盖，但是不能是打开状态，会报错
 
 for i in os.listdir():
